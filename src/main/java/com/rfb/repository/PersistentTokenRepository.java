ef892affe31fbf5e579e7b4dbc1b85008b88fa7d
Yu-Chi Lai <yuchilai88@gmail.com>
2021-04-04 23:42:52 -0400

    first commit

---
+++ src/main/java/com/rfb/repository/PersistentTokenRepository.java
@@ -0,0 +1,16 @@
+package com.rfb.repository;
+
+import com.rfb.domain.PersistentToken;
+import com.rfb.domain.User;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link PersistentToken} entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+}
